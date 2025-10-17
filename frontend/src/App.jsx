1810480c20e6f6a066c9b2d94aba29f896ceedd5
Praneeth860 <mpraneeth474@gmail.com>
2025-10-17 07:38:18 +0530

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <div className="navbar-search-icon">
+          <img src={assets.search_icon} alt="" />
+        </div>
+        <img src={assets.basket_icon} alt="" />
+        <div className="dot"></div>
+        <button>sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
